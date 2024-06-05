233e88e28c1d6965c11973d46748b4aaec36b208
love sharma <73900102+lokesharma95@users.noreply.github.com>
2024-06-05 21:28:14 +0530

    LC: Merge Two Sorted Lists (Problem 21)

---
+++ LC_21.java
@@ -0,0 +1,43 @@
+public class LC_21 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+        return list1 == null ? list2 : list1;
+    }
+
+    public static void main(String[] args) {
+        LC_21 lc = new LC_21();
+
+        // Example linked lists:
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode mergedList = lc.mergeTwoLists(list1, list2);
+        System.out.print("Merged List: ");
+        printList(mergedList);
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
