7474073c4dcbdbd35ca83e700b384a995441fb61
love sharma <73900102+lokesharma95@users.noreply.github.com>
2024-06-09 15:49:09 +0530

    LC: Remove Duplicates from Sorted List

---
+++ LC_83.java
@@ -0,0 +1,64 @@
+public class LC_83 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode res = head;
+
+        while (head != null && head.next != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LC_83 solution = new LC_83();
+
+        // Create the linked list 1 -> 1 -> 2 -> 3 -> 3
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        // Print the list before deletion
+        System.out.print("Original list: ");
+        printList(head);
+
+        // Delete duplicates
+        ListNode result = solution.deleteDuplicates(head);
+
+        // Print the list after deletion
+        System.out.print("List after removing duplicates: ");
+        printList(result);
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
