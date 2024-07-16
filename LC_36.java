757cfd1232816d4e7cca7d0074f2810d7d1ef499
love sharma <73900102+lokesharma95@users.noreply.github.com>
2024-07-17 01:28:10 +0530

    LC: Valid Sudoku

---
+++ LC_36.java
@@ -0,0 +1,47 @@
+
+public class LC_36 {
+
+    public boolean isValidSudoku(char[][] board) {
+        int n = board.length;
+        boolean[][] rows = new boolean[n][n];
+        boolean[][] cols = new boolean[n][n];
+        boolean[][] boxes = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + j / 3;
+
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                        return false;
+                    }
+
+                    rows[i][num] = true;
+                    cols[j][num] = true;
+                    boxes[boxIndex][num] = true;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LC_36 lc = new LC_36();
+        char[][] example1 = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        boolean result = lc.isValidSudoku(example1);
+        System.out.println("Result: " + result);
+    }
+}
