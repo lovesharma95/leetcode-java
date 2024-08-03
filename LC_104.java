bea71365c2f91f11ff72d94ebb60651da7b76dab
love sharma <73900102+lokesharma95@users.noreply.github.com>
2024-08-04 01:25:27 +0530

    LC: Maximum Depth of Binary Tree

---
+++ LC_104.java
@@ -0,0 +1,48 @@
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class LC_104 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static void main(String[] args) {
+        LC_104 lc = new LC_104();
+
+        // Manually creating the tree [3,9,20,null,null,15,7]
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Maximum Depth: ");
+        System.out.println(lc.maxDepth(root));
+    }
+}
